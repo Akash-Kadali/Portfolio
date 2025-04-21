772d46b61ff4b21c2ff2b007f46c2cdcdb6ef9bb
Sri Akash Kadali <kadali18@umd.edu>
2025-04-21 04:14:33 -0400

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,33 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleResize = (e) => setIsMobile(e.matches);
+    mediaQuery.addEventListener("change", handleResize);
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          {isMobile ? (
+            <img
+              src={technology.icon}
+              alt={technology.name}
+              className='w-full h-full object-contain'
+            />
+          ) : (
+            <BallCanvas icon={technology.icon} />
+          )}
         </div>
       ))}
     </div>
