0ff692c1458278c430d5b006b8ab48ac6d48364a
Sri Akash Kadali <kadali18@umd.edu>
2025-04-24 02:27:51 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Kaushik &nbsp;
-            <span className='sm:block hidden'> | Portfolio</span>
+            Sri &nbsp;
+            <span className='sm:block hidden'> | Kadali</span>
           </p>
         </Link>
 
