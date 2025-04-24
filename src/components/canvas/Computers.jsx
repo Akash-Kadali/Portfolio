6b4af1e34ab2d916fbfd6edbe238ac56841fe2e7
Sri Akash Kadali <kadali18@umd.edu>
2025-04-24 19:16:35 -0400

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,87 +1,78 @@
-import React from 'react'
-import { Suspense, useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from '../Loader.jsx';
 
+import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("../desktop_pc/scene.gltf")
-
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight 
-            intensity={0.15}
-            groundColor='black'
+      <hemisphereLight intensity={0.95} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
       />
-      <pointLight 
-            intensity={1} />
-      <spotLight 
-            angle={0.12}
-            position={[-20, 50, 10]} 
-            penumbra={1}
-            intensity={1}
-            castShadow
-            shadow-mapSize-width={1024}
-            />
-      <primitive 
-            object={computer.scene} 
-            scale={isMobile ? 0.7 : 0.75}
-            position={isMobile ? [0, -3, -2.2] : [0, -4, -1.5]}
-            rotation={[-0.01, -0.2, -0.1]}
+      <pointLight intensity={2} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.2 : 0.25}
+        position={isMobile ? [-1, -1.5, -2] : [2, -3.4, -0.5]}
+        rotation={[-0.00, 0.8, -0.0]}
       />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
-  const [isMobile, setisMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add event listener for changes to the screen size
-    const mediaQuery = window.matchMedia(
-      '(max-width: 500px)');
-      
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-      // Set the initial value of isMobile state variable
-      setisMobile(mediaQuery.matches);
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
 
-      
-      //Define a callback function to handle changes to the media query
-      const handleMediaQueryChange = (event) => {
-        setisMobile(event.matches);
-      };
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-      // Add the callback function as a listener for changes to the media query
-      mediaQuery.addEventListener('change', handleMediaQueryChange);
-      
-
-      // Remove the event listener when the component unmounts
-      return () => {
-        mediaQuery.removeEventListener('change', handleMediaQueryChange);
-      }
-  }, [])
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <Canvas
-    frameLoop='demand'
-    shadows
-    camera={{ position: [20, 3, 5], fov: 25 }}
-    gl={{ preserveDrawingBuffer: true }}
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        enableZoom={false} 
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
+
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
 export default ComputersCanvas;
